e018f76ac6b294c8730fe3f0c04bdf6899c522b9
sandruh <43049109+sandruh@users.noreply.github.com>
2018-12-06 15:19:03 -0500

    Add files via upload

--- concerts2/script.js
+++ concerts2/script.js
@@ -1,5 +1,79 @@
 $(document).ready(function() {
   console.log("ready.");
-  $(".panic1").click(function() {
-    $(".panic1").removeClass("panic1").addClass("panictitle");  
+  $(".panic1").hover(function() {
+    $(".panic1").toggleClass("panictitle");  
+    $(".backgroundphoto").toggleClass()
   });
+});
+
+$(document).ready(function() {
+  console.log("ready.");
+  $(".panic2").hover(function() {
+    $(".panic2").toggleClass("panictitle");  
+    $(".backgroundphoto").toggleClass()
+  });
+});
+
+$(document).ready(function() {
+  console.log("ready.");
+  $(".panic3").hover(function() {
+    $(".panic3").toggleClass("panictitle");  
+    $(".backgroundphoto").toggleClass()
+  });
+});
+
+$(document).ready(function() {
+  console.log("ready.");
+  $(".top1").hover(function() {
+    $(".top1").toggleClass("toptitle");  
+    $(".backgroundphoto").toggleClass()
+  });
+});
+
+$(document).ready(function() {
+  console.log("ready.");
+  $(".top2").hover(function() {
+    $(".top2").toggleClass("toptitle");  
+    $(".backgroundphoto").toggleClass()
+  });
+});
+
+$(document).ready(function() {
+  console.log("ready.");
+  $(".top3").hover(function() {
+    $(".top3").toggleClass("toptitle");  
+    $(".backgroundphoto").toggleClass()
+  });
+});
+
+$(document).ready(function() {
+  console.log("ready.");
+  $(".wtm1").hover(function() {
+    $(".wtm1").toggleClass("wtmtitle");  
+    $(".backgroundphoto").toggleClass()
+  });
+});
+
+$(document).ready(function() {
+  console.log("ready.");
+  $(".wtm2").hover(function() {
+    $(".wtm2").toggleClass("wtmtitle");  
+    $(".backgroundphoto").toggleClass()
+  });
+});
+
+$(document).ready(function() {
+  console.log("ready.");
+  $(".bleachers1").hover(function() {
+    $(".bleachers1").toggleClass("bleacherstitle");  
+    $(".backgroundphoto").toggleClass()
+  });
+});
+
+$(document).ready(function() {
+  console.log("ready.");
+  $(".bleachers2").hover(function() {
+    $(".bleachers2").toggleClass("bleacherstitle");  
+    $(".backgroundphoto").toggleClass()
+  });
+});
