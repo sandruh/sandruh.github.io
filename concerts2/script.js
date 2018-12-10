3a8f201544e5060011da949328ded2c83cb50e9e
sandruh <43049109+sandruh@users.noreply.github.com>
2018-12-10 11:11:31 -0500

    Add files via upload

--- concerts2/script.js
+++ concerts2/script.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   console.log("ready.");
   $(".panic1").hover(function() {
     $(".panic1").toggleClass("panictitle");  
-    $(".backgroundphoto").toggleClass()
   });
 });
 
@@ -10,7 +9,6 @@ $(document).ready(function() {
   console.log("ready.");
   $(".panic2").hover(function() {
     $(".panic2").toggleClass("panictitle");  
-    $(".backgroundphoto").toggleClass()
   });
 });
 
@@ -18,15 +16,14 @@ $(document).ready(function() {
   console.log("ready.");
   $(".panic3").hover(function() {
     $(".panic3").toggleClass("panictitle");  
-    $(".backgroundphoto").toggleClass()
   });
 });
+
 
 $(document).ready(function() {
   console.log("ready.");
   $(".top1").hover(function() {
     $(".top1").toggleClass("toptitle");  
-    $(".backgroundphoto").toggleClass()
   });
 });
 
@@ -34,7 +31,6 @@ $(document).ready(function() {
   console.log("ready.");
   $(".top2").hover(function() {
     $(".top2").toggleClass("toptitle");  
-    $(".backgroundphoto").toggleClass()
   });
 });
 
@@ -42,23 +38,6 @@ $(document).ready(function() {
   console.log("ready.");
   $(".top3").hover(function() {
     $(".top3").toggleClass("toptitle");  
-    $(".backgroundphoto").toggleClass()
-  });
-});
-
-$(document).ready(function() {
-  console.log("ready.");
-  $(".wtm1").hover(function() {
-    $(".wtm1").toggleClass("wtmtitle");  
-    $(".backgroundphoto").toggleClass()
-  });
-});
-
-$(document).ready(function() {
-  console.log("ready.");
-  $(".wtm2").hover(function() {
-    $(".wtm2").toggleClass("wtmtitle");  
-    $(".backgroundphoto").toggleClass()
   });
 });
 
@@ -66,7 +45,6 @@ $(document).ready(function() {
   console.log("ready.");
   $(".bleachers1").hover(function() {
     $(".bleachers1").toggleClass("bleacherstitle");  
-    $(".backgroundphoto").toggleClass()
   });
 });
 
@@ -74,6 +52,19 @@ $(document).ready(function() {
   console.log("ready.");
   $(".bleachers2").hover(function() {
     $(".bleachers2").toggleClass("bleacherstitle");  
-    $(".backgroundphoto").toggleClass()
+  });
+});
+
+$(document).ready(function() {
+  console.log("ready.");
+  $(".wtm1").hover(function() {
+    $(".wtm1").toggleClass("wtmtitle");  
+  });
+});
+
+$(document).ready(function() {
+  console.log("ready.");
+  $(".wtm2").hover(function() {
+    $(".wtm2").toggleClass("wtmtitle");  
   });
 });
